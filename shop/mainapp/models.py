a4a9f3110cd08dd2092dd2f5a6f2a9d839d86d6c
Nirsuss <47823839+Nirsuss@users.noreply.github.com>
2020-11-23 15:17:24 +0500

    remade application architecture

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 User = get_user_model()
 
@@ -27,6 +28,10 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+
+    class Meta:
+        abstract = True
+
     category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
     title = models.CharField(max_length=255, verbose_name='Наименование')
     slug = models.SlugField(unique=True)
@@ -41,7 +46,9 @@ class Product(models.Model):
 class CartProduct(models.Model):
     user = models.ForeignKey('Customer', verbose_name='Покупатель', on_delete=models.CASCADE)
     cart = models.ForeignKey('Cart', verbose_name='Корзина', on_delete=models.CASCADE, related_name='related_products')
-    product = models.ForeignKey(Product, verbose_name='Товар', on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
     qty = models.PositiveIntegerField(default=1)
     finals_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая Цена')
 
@@ -68,10 +75,4 @@ class Customer(models.Model):
         return "Покупатель: {} {}".format(self.user.first_name, self.user.last_name)
 
 
-class Specification(models.Model):
-    content = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    name = models.CharField(max_length=255, verbose_name='Имя товара для характеристика')
 
-    def __str__(self):
-        return "Характеристика для товара".format(self.name)
